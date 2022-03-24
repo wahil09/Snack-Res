df900ad0a4117c33a3be42e717a81300ad822682
wahil chettouf <wahilchettouf@gmail.com>
2022-03-24 06:25:01 +0100

    Ajouter les fichiers : HTML, CSS, images

---
+++ views/view_all_burger.php
@@ -0,0 +1,53 @@
+<section class="row"> <!-- tail flexabol 100% -->
+    <article class="art-0">
+        <div class="conatainer-art">
+            <div class="dit-title-art">
+                <h2 class="title-art">Nous Plats</h2>
+            </div>
+            <div class="all-burgers">
+                <div class="burger">
+                    <div class="burger-img">
+                        <img src="public/images/burger-noir." alt="burger-image">
+                    </div>
+                    <div class="info-burger">
+                        <p class="descp-burger">
+                            Lorem ipsum dolor sit amet consectetur adipisicing.
+                        </p>
+                    </div>
+                    <div class="func-burger">
+                        <a href="#" class="btn-burger btn-command">commander</a>
+                        <a href="#" class="btn-burger btn-info">ajouter</a>
+                    </div>
+                </div>
+                <div class="burger">
+                    <div class="burger-img">
+                        <img src="public/images/burger-poullet." alt="burger-image">
+                    </div>
+                    <div class="info-burger">
+                        <p class="descp-burger">
+                            Lorem ipsum dolor sit amet consectetur adipisicing.
+                        </p>
+                    </div>
+                    <div class="func-burger">
+                        <a href="#" class="btn-burger btn-command">commander</a>
+                        <a href="#" class="btn-burger btn-info">ajouter</a>
+                    </div>
+                </div>
+                <div class="burger">
+                    <div class="burger-img">
+                        <img src="public/images/burger." alt="burger-image">
+                    </div>
+                    <div class="info-burger">
+                        <p class="descp-burger">
+                            Lorem ipsum dolor sit amet consectetur adipisicing.
+                        </p>
+                    </div>
+                    <div class="func-burger">
+                        <a href="#" class="btn-burger btn-command">command</a>
+                        <a href="#" class="btn-burger btn-info">ajouter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </article>
+</section>
